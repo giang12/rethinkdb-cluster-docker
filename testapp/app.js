823c80aa48118c496b98255890003300623049da
Zen Nguyen <nggiang12@gmail.com>
2018-06-05 21:55:04 -0700

    set repo
    zen12/rethinkdb-cluster
    testapp exit after running fine ~2mins
    swarm mode

--- testapp/app.js
+++ testapp/app.js
@@ -1,30 +1,50 @@
-'use strict';
+"use strict";
 
-console.log('I\'m starting fresh');
+console.log("I'm starting fresh");
 
-const r = require('rethinkdbdash')({
-  servers: [{
-    host: 'rethinkdb1',
-    port: 28015
-  }, {
-    host: 'rethinkdb2',
-    port: 28015
-  }, {
-    host: 'rethinkdb3',
-    port: 28015
-  }, {
-    host: 'rethinkdb4',
-    port: 28015
-  }, {
-    host: 'rethinkdb5',
-    port: 28015
-  }],
+const r = require("rethinkdbdash")({
+  servers: [
+    {
+      host: "rethinkdb1",
+      port: 28015
+    },
+    {
+      host: "rethinkdb2",
+      port: 28015
+    },
+    {
+      host: "rethinkdb3",
+      port: 28015
+    },
+    {
+      host: "rethinkdb4",
+      port: 28015
+    },
+    {
+      host: "rethinkdb5",
+      port: 28015
+    }
+  ],
   timeout: 5000
 });
 
+const thresh = 12;
+let count = 0;
+
 setInterval(() => {
-  r.db('clustertest').table('importantData')
+  if (count > thresh) {
+    console.info("cluster is running normal, exitting testapp..");
+
+    return process.exit(0);
+  }
+
+  r.db("clustertest")
+    .table("importantData")
     .then(result => {
-      console.log('read', result);
+      count++;
+      console.log("read clustertest.importantData", result);
+    })
+    .catch(err => {
+      console.error(err);
     });
 }, 10000);
